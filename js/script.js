b42665af6cf03aa5d26e9546f6b5d1a09934bec7
mike v <xvx1138@protonmail.com>
2019-06-04 21:30:27 +0200

    other job role

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 $( "#name" ).focus();
 
 // ”Job Role” section
+$('#title').click( function () {
+  
+});
+/**
+$('#mySelectBox option').each(function() {
+    if($(this).isChecked())
+       alert('this option is selected');
+     else
+       alert('this is not');
+});
+**/
+
+$( "#other-title" ).hide();
