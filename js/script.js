dfb40b13b72d3e9f441732ea293128b330f8de67
mike v <xvx1138@protonmail.com>
2019-06-06 22:47:59 +0200

    created select tshirt theme

--- js/script.js
+++ js/script.js
@@ -24,7 +24,26 @@ $('#title').on('click keypress', function () {
 //=======================================
 // T-shirt section
 // ======================================
-
+// Hide the “Select Theme” `option` element in the “Design” menu.
+// $('#design option').eq(0).hide();
+// Update the “Color” field to read “Please select a T-shirt theme”.
+const $color = $('<option>Please select a T-shirt theme</option>');
+$('#color').prepend($color);
 // "Theme - JS Puns" colors: "Cornflower Blue," "Dark Slate Grey," and "Gold."
-// $('#color option').eq(0).css({color: red});
+$('#design').on('click keypress', function (e) {
+  const theme = $(e.target).val();
+  if (theme === 'js puns') {
+    $('#color option').eq(6).hide();
+    $('#color option').eq(5).hide();
+    $('#color option').eq(4).hide();
+  } else if (theme === 'heart js') {
+    $('#color option').eq(3).hide();
+    $('#color option').eq(2).hide();
+    $('#color option').eq(1).hide();
+  }
+  else {
+    $('#color option').show();
+  }
+});
+
 // "Theme - I ♥ JS" colors: "Tomato," "Steel Blue," and "Dim Grey."
