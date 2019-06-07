b9b876aa16543e58cf701bf236a145cf12ae411e
mike v <xvx1138@protonmail.com>
2019-06-07 11:32:51 +0200

    tshirt section

--- js/script.js
+++ js/script.js
@@ -25,25 +25,56 @@ $('#title').on('click keypress', function () {
 // T-shirt section
 // ======================================
 // Hide the “Select Theme” `option` element in the “Design” menu.
-// $('#design option').eq(0).hide();
+$('#design').on('click keypass change', function () {
+  $('#design option').eq(0).hide();
+});
 // Update the “Color” field to read “Please select a T-shirt theme”.
 const $color = $('<option>Please select a T-shirt theme</option>');
 $('#color').prepend($color);
-// "Theme - JS Puns" colors: "Cornflower Blue," "Dark Slate Grey," and "Gold."
-$('#design').on('click keypress', function (e) {
-  const theme = $(e.target).val();
+
+
+// $('#design').change( function (e) {
+//   const theme = $(this).val();
+//   if (theme === 'js puns') {
+//     $('#color ').eq(6).hide();
+//     $('#color ').eq(5).hide();
+//     $('#color ').eq(4).hide();
+//   } else if (theme === 'heart js') {
+//     $('#color ').eq(3).hide();
+//     $('#color ').eq(2).hide();
+//     $('#color ').eq(1).hide();
+//   }
+//   else {
+//     $('#color ').hide();
+//   }
+// });
+
+$('#design').change(function (e) {
+  const theme = $(this).val();
   if (theme === 'js puns') {
+    $('#color option').eq(0).hide();
     $('#color option').eq(6).hide();
     $('#color option').eq(5).hide();
     $('#color option').eq(4).hide();
-  } else if (theme === 'heart js') {
+  }
+  else {
+    $('#color option').eq(6).show();
+    $('#color option').eq(5).show();
+    $('#color option').eq(4).show();
+  }
+});
+
+$('#design').change(function (e) {
+  const theme = $(this).val();
+  if (theme === 'heart js') {
+    $('#color option').eq(0).hide();
     $('#color option').eq(3).hide();
     $('#color option').eq(2).hide();
     $('#color option').eq(1).hide();
   }
   else {
-    $('#color option').show();
+    $('#color option').eq(3).show();
+    $('#color option').eq(2).show();
+    $('#color option').eq(1).show();
   }
 });
-
-// "Theme - I ♥ JS" colors: "Tomato," "Steel Blue," and "Dim Grey."
