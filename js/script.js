e18fc53ebe350c5556c9265d55be08c35802ada9
mike v <xvx1138@protonmail.com>
2019-06-21 15:22:23 +0200

    clean up to focus on fixes

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $( document ).ready(function() {
 $( "#other-title" ).hide();
 // display other input when selected
 $('#title').on('click keypress', function () {
-  const selected = $(this).val(); // .val() - Get the current value of the first element in the set of matched elements.
+  let selected = $(this).val(); // .val() - Get the current value of the first element in the set of matched elements.
   if (selected === 'other') {
     $( "#other-title" ).show();
   } else {
@@ -38,13 +38,15 @@ $('#color').prepend($color).val('default');
 $('#color option').hide();
 // when a theme is selected colors is given to correct theme
 $('#design').change(function (e) {
-  const theme = $(this).val();
+  let theme = $(this).val();
   const $jsPuns = $( "#color option:contains('JS Puns')" );
   const $heartJs = $( "#color option:contains('I')" );
   if (theme === 'js puns' ) {
+    $jsPuns.eq(0).prop('selected', true );
     $jsPuns.show();
     $heartJs.hide();
   } else if ( theme === 'heart js' ) {
+    $heartJs.eq(0).prop('selected', true );
     $heartJs.show();
     $jsPuns.hide();
   } else {
@@ -83,79 +85,75 @@ $('.activities').change(function (e) {
     // else subtract the cost.
     cost -= price;
   }
-  // set the text of the total cost element equal to the string ‘Total: $’
-  // concatenated with the current value of the total cost variable
+  // set the text of the total cost element equal to the string ‘Total: $’ concatenated with the current value of the total cost variable
   let $totalCost = $total.text('Total: $ ' + cost).css({"color": "green", "font-weight": "bold", "font-size": "20px"});
 // Disabling conflicting activities part 1 ... ----------------
-  const emDash = '—';
-  const comma = ',';
-  const ioDash = $activity.indexOf(emDash) + 1;
-  const ioComma = $activity.indexOf(comma);
-  const dayNtime = $activity.slice(ioDash, ioComma);
-
+  // let $clicked = $(e.target);
+  //  $( $clicked ).prop( "checked" )
+  // let $activity = $clicked.parent().text();
+  let emDash = '—';
+  let comma = ',';
+  let ioDash = $activity.indexOf(emDash) + 1;
+  let ioComma = $activity.indexOf(comma);
+  let dayNtime = $activity.slice(ioDash, ioComma);
 // Disabling conflicting activities part 2 ... -----------------------
 //-------------------------
 // BELOW HERE, HELP MEEEE !!!
 //-------------------------
-  // The .each() method is designed to make DOM looping constructs
-  // concise and less error-prone.the keyword this refers to the element
-
-  // the element is $(activities input), this is where 'this' comes in
-  // const times =  $('.activities input').each(function (i, element){
-  //     const $clickedActivity = $( this ).prop( "checked" );
-  //     // console.log($clickedActivity);
-  //     if ( true && true ) {
-  //       if ( true ) {
-  //           $clickedActivity.disabled = false;
-  //         } else {
-  //           $clickedActivity.disabled = true;
-  //         }
-  //     }
-  //   });
-  //   console.log(times);
+// The .each() method is designed to make DOM looping constructs concise and less error-prone.the keyword this refers to the element
+// the element is $(activities input), this is where 'this' comes in
+  // $('.activities input').each(function (i, element){
+    // variables
+    // if ( something === something && something !== something ) {
+    //   if ( $clickedActivity ) {
+    //       something.disabled = false;
+    //     } else {
+    //       something.disabled = true;
+    //     }
+    // }
+  // });
 
 // variables to select activity checkboxes
-  const $jsFrameworks = $("input[name = 'js-frameworks']");
-  const $jsLibs = $("input[name = 'js-libs']");
-  const $express = $("input[name = 'express']");
-  const $node = $("input[name = 'node']");
-
-// if js-frameworks is checked, stop express from being checked
-  $( $jsFrameworks ).change(function() {
-      if ( $( this ).prop( "checked" ) ){
-          $express.prop("disabled", true);
-      } else {
-          $express.prop("disabled", false);
-      }
-  });
-// if express is checked, stop js-frameworks from being checked
-  $($express).change(function() {
-      if ( $( this ).prop( "checked" ) ) {
-          $jsFrameworks.prop("disabled", true);
-      } else {
-          $jsFrameworks.prop("disabled", false);
-      }
-  });
-  // if js-libs is checked, stop node from being checked
-  $($jsLibs).change(function() {
-      if ( $( this ).prop( "checked" ) ) {
-          $node.prop("disabled", true);
-      } else {
-          $node.prop("disabled", false);
-      }
-  });
-  // if node is checked, stop js-libs from being checked
-  $($node).change(function() {
-      if ( $( this ).prop( "checked" )) {
-          $jsLibs.prop("disabled", true);
-      } else {
-          $jsLibs.prop("disabled", false);
-      }
-  });
+//   const $jsFrameworks = $("input[name = 'js-frameworks']");
+//   const $jsLibs = $("input[name = 'js-libs']");
+//   const $express = $("input[name = 'express']");
+//   const $node = $("input[name = 'node']");
+//
+// // if js-frameworks is checked, stop express from being checked
+//   $( $jsFrameworks ).change(function() {
+//       if ( $( this ).prop( "checked" ) ){
+//           $express.prop("disabled", true);
+//       } else {
+//           $express.prop("disabled", false);
+//       }
+//   });
+// // if express is checked, stop js-frameworks from being checked
+//   $($express).change(function() {
+//       if ( $( this ).prop( "checked" ) ) {
+//           $jsFrameworks.prop("disabled", true);
+//       } else {
+//           $jsFrameworks.prop("disabled", false);
+//       }
+//   });
+//   // if js-libs is checked, stop node from being checked
+//   $($jsLibs).change(function() {
+//       if ( $( this ).prop( "checked" ) ) {
+//           $node.prop("disabled", true);
+//       } else {
+//           $node.prop("disabled", false);
+//       }
+//   });
+//   // if node is checked, stop js-libs from being checked
+//   $($node).change(function() {
+//       if ( $( this ).prop( "checked" )) {
+//           $jsLibs.prop("disabled", true);
+//       } else {
+//           $jsLibs.prop("disabled", false);
+//       }
+//   });
 
 // dont pass / end of activity function --------------
 });
-
 //--------------------------------------------
 // Payment Section
 //--------------------------------------------
@@ -164,13 +162,12 @@ $('#payment').change( function () {
   $('#payment option').eq(0).hide();
 });
 $('option[value="credit card"]').attr('selected', true);
-
+const $credit = $( '#credit-card' );
 const $paypal = $( "div p:contains('Paypal')" ).hide();
 const $bitcoin = $( "div p:contains('Bitcoin')" ).hide();
 // payment selections
 $('#payment').change(function () {
-  const payment = $(this).val();
-  const $credit = $( '#credit-card' );
+  let payment = $(this).val();
   if (payment === 'paypal' ) {
     $credit.hide();
     $paypal.show();
@@ -194,32 +191,42 @@ $(document).ready(function () {
 //--------------------------------------------
 // Form Validation
 //--------------------------------------------
-// email validate
+// REGEX VALIDATION
+// const emailRegex = /^\w+@\w+\..{3}?$/;
+// const cardRegex = /^\d{13,16}$/;
+// const zipRegex = /^\d{5}$/;
+// const cvvRegex = /^\d{3}$/;
+
+// EMAIL
 function validEmail ( email ) {
   const emailRegex = /^\w+@\w+\..{3}?$/;
   return emailRegex.test( email );
 }
+// CREDIT CARD
+function isValidCreditCard (number) {
+  const cardRegex = /^\d{13,16}$/;
+  return cardRegex.test(number);
+}
+function isValidZip (number) {
+  const zipRegex = /^\d{5}$/;
+  return zipRegex.test(number);
+}
+function isValidCVV (number) {
+  const cvvRegex = /^\d{3}$/;
+  return cvvRegex.test(number);
+}
 
 // REGISTER BUTTON VALIDATION
-const $nameLetters = $('label[for="name"]');
-const $emailLetters = $('label[for="mail"]');
-const $ccNum = $('label[for="cc-num"]');
-const $zip = $('label[for="zip"]');
-const $cvv = $('label[for="cvv"]');
-// regex
-const cardRegex = /^\d{13,16}$/;
-const zipRegex = /^\d{5}$/;
-const cvvRegex = /^\d{3}$/;
-const cardVal = $('#cc-num').val();
-const zipVal = $('#zip').val();
-const cvvVal = $('#cvv').val();
-
 // complete fields warning
 const $warning = $('<label></label>');
 $('form').append($warning);
 const $incomplete = $warning.text('Fill in empty fields').css({"color": "red", "font-weight": "bold"});
 $incomplete.hide();
-
+const $nameLetters = $('label[for="name"]');
+const $emailLetters = $('label[for="mail"]');
+const $ccNum = $('label[for="cc-num"]');
+const $zip = $('label[for="zip"]');
+const $cvv = $('label[for="cvv"]');
 // Register Button
 $( 'button' ).on( 'click', function(e) {
 // name input
@@ -237,26 +244,26 @@ $( 'button' ).on( 'click', function(e) {
     $incomplete.show();
   }
 // activity section
-  if ( !$( '.activities input' ).prop( "checked" ) ) {
+  if ( $( '.activities input' ).prop( "checked" ) ) {
     $('.activities legend').css({"color": "red", "font-weight": "bold"});
     e.preventDefault();
     $incomplete.show();
   }
 // credit card
   if ( $('#payment').val() === 'credit card' ) {
-    if ( $('#cc-num').val() === "" || (cardRegex.exec(cardVal)) === null) {
+    if ( $('#cc-num').val() === "" ) {
       $ccNum.css({"color": "red", "font-weight": "bold"});
       $('#cc-num').css({"border-color": "red"});
       e.preventDefault();
       $incomplete.show();
     }
-    if ( $('#zip').val() === "" || (zipRegex.exec(zipVal)) === null ) {
+    if ( $('#zip').val() === "" ) {
       $zip.css({"color": "red", "font-weight": "bold"});
       $('#zip').css({"border-color": "red"});
       e.preventDefault();
       $incomplete.show();
     }
-    if ($('#cvv').val() === "" || (cvvRegex.exec(cvvVal)) === null ) {
+    if ($('#cvv').val() === "" ) {
       $cvv.css({"color": "red", "font-weight": "bold"});
       $('#cvv').css({"border-color": "red"});
       e.preventDefault();
